bafe73c1b3097744d4707dc426032a4a32656321
weto <weto.jc@gmail.com>
2019-06-11 21:16:47 -0300

    add pagina of

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var port = 3000
+
+var server = app.listen(port, function() {
+    console.log("1.0.0");
+	console.log("PWA no ar");
+	console.log("No agoraaa");
+});
+
+module.exports = app;
